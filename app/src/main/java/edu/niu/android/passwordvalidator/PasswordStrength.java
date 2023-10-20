89fcdd7b0ac28b9f8f13cebd16eb63b9d0ea2edc
Mark <z058227@students.niu.edu>
2023-10-20 15:53:19 -0500

    App Completed

---
+++ app/src/main/java/edu/niu/android/passwordvalidator/PasswordStrength.java
@@ -0,0 +1,19 @@
+/*******************************************************************************************
+ *                                                                                         *
+ *  Class Name: PasswordStrength                                                           *
+ *                                                                                         *
+ *     Purpose: This class is the model for the app that has one method that returns       *
+ *              a boolean that indicates whether or not the password is strong (8 or more  *
+ *              characters) or weak (less than 8 characters)                               *
+ *                                                                                         *
+ ******************************************************************************************/
+package edu.niu.android.passwordvalidator;
+
+public class PasswordStrength {
+
+    // Determine whether or not the password is strong
+    public boolean weakPass(String password) {
+        return password.length() <= 8;
+    }
+
+}
